70e546c9076ad1a261dff50c816e4ff953812532
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-06-21 00:34:16 +0530

    Day-2: Understanding react flow and project structure

---
+++ Day-2/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Coffee from "./Coffee"
+
+function App() {
+  return (
+    <>
+      <Coffee/>
+      <p>you can only return one element so either wrap all other in div or use fragment(write div and remove name just keep greater and lower tha sign)
+
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ Day-2/vite-project/src/Coffee.jsx
@@ -0,0 +1,6 @@
+function Coffee(){
+    return(
+        <h2>Coffee is ready</h2>
+    )
+}
+export default Coffee
