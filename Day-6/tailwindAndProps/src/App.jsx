17ff630bdbd017096c89b2ca4bf3e2fba8600771
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-06-23 00:14:50 +0530

    Tailwind & Props

---
+++ Day-6/tailwindAndProps/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import Card from './components/Card'
+
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-green-800 text-black p-4 rounded-xl'>Tailwind Test!!</h1>
+
+
+      <Card username='abhishek' btnTxt='read-more'/>
+      <Card username='kunal'/>
+      
+    
+    </>
+  )
+}
+
+export default App
