d4112363900f60c71464e6c732e9a2971590e21c
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2026-06-23 12:24:10 +0530

    Day-10: Currency Converter(Custom hooks)

---
+++ Day-10/currency-converter/src/hooks/useCurrencyinfo.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+
+function useCurrencyInfo(currency){
+
+    const [data, setData] = useState({})
+
+    // response from api mostly come in strings convert them to json
+
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => {
+            if (res && res[currency]) {
+                setData(res[currency])
+            } else {
+                setData({})
+            }
+        })
+        .catch(() => {
+            setData({})
+        })
+    }, [currency])
+
+    return data;
+}
+
+export default useCurrencyInfo;
